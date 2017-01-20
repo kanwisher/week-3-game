27914147117f6601e9847cbbed6b2907c42317de
kanwisher <kanwisher@gmail.com>
2017-01-20 00:15:14 -0500

    Update game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -47,35 +47,35 @@ document.onkeyup = function() { //if a key is pressed
         for (i = 0; i < hangMan.computerWordChoice.length; i++) {
 
             if (hangMan.computerWordChoice.charAt(i) === userLetterChoice) { //runs through each letter of the word the computer chose, if any letter in the word  is the same as the guessed letter
-                underscoreArray[i] = userLetterChoice; //then change in the underScore array from an underscore to the guessed letter, like a replacement ( computerWordChoice.charAt(i) will always be in relation to the same position in underscoreArray[i])
+                hangMan.underscoreArray[i] = userLetterChoice; //then change in the underScore array from an underscore to the guessed letter, like a replacement ( computerWordChoice.charAt(i) will always be in relation to the same position in underscoreArray[i])
                 updateStats(); //update the stats so we can see the changes
             }
         }
 
-        if (underscoreArray.indexOf(userLetterChoice) === -1) { // Part of the final else statement. Only runs if it's an incorrect guess that hasn't been used before
-            usedLetters.push(userLetterChoice); //Push the wrong letter guess into the UsedLetters array, we can test now if these have already been guessed if they appear in the array on future guesses
-            lives -= 1; //take away a guess for being wrong
+        if (hangMan.underscoreArray.indexOf(userLetterChoice) === -1) { // Part of the final else statement. Only runs if it's an incorrect guess that hasn't been used before
+            hangMan.usedLetters.push(userLetterChoice); //Push the wrong letter guess into the UsedLetters array, we can test now if these have already been guessed if they appear in the array on future guesses
+            hangMan.lives -= 1; //take away a guess for being wrong
             updateStats(); //update stats so we can see the changes
         }
 
-        if (underscoreArray.indexOf("_") == -1) { //Run if there aren't any underscores remaining in the array. If there aren't any underscores left, then the puzzle has been solved
+        if (hangMan.underscoreArray.indexOf("_") === -1) { //Run if there aren't any underscores remaining in the array. If there aren't any underscores left, then the puzzle has been solved
             
-            keyLock = true;
+            hangMan.keyLock = true;
             message = "You win!"
             document.getElementById("message").innerHTML = message;
-            ++wins;
-            document.getElementById("wins").innerHTML = "Wins: " + wins;
+            ++hangMan.wins;
+            document.getElementById("wins").innerHTML = "Wins: " + hangMan.wins;
             
 
         }
 
-        if (lives == 0) { //Run if guesses run out
+        if (hangMan.lives === 0) { //Run if guesses run out
             
-            keyLock = true;
+            hangMan.keyLock = true;
             message = "Game Over"
             document.getElementById("message").innerHTML = message;
-            ++losses;
-            document.getElementById("losses").innerHTML = "Losses: " + losses;
+            ++hangMan.losses;
+            document.getElementById("losses").innerHTML = "Losses: " + hangMan.losses;
             
 
         }
@@ -85,30 +85,30 @@ document.onkeyup = function() { //if a key is pressed
 
 
 function resetGame() {
-    usedLetters = [];
-    underscoreArray = [];
-    underscoreString = "";
-    keyLock = false; //unlocks keyboard
-    lives = 10;
-    generatePuzzle();
+    hangMan.usedLetters = [];
+    hangMan.underscoreArray = [];
+    hangMan.underscoreString = "";
+    hangMan.keyLock = false; //unlocks keyboard
+    hangMan.lives = 10;
+    hangMan.generatePuzzle();
     updateStats();
 
 };
 
 function generatePuzzle() {
-    computerWordChoice = wordArray[Math.floor(Math.random() * wordArray.length)]; //computer selects a random word
-    for (i = 0; i < computerWordChoice.length; i++) { //creates array of underscores so I can pull their index to match with correct letters
-        underscoreArray.push("_");
+    hangMan.computerWordChoice = hangMan.wordArray[Math.floor(Math.random() * hangMan.wordArray.length)]; //computer selects a random word
+    for (i = 0; i < hangMan.computerWordChoice.length; i++) { //creates array of underscores so I can pull their index to match with correct letters
+        hangMan.underscoreArray.push("_");
     }
 };
 
 
 function updateStats() {
-    var underscoreString = underscoreArray.join(" "); //Sets my array as a string and adds a space inbetween each item
+    var underscoreString = hangMan.underscoreArray.join(" "); //Sets my array as a string and adds a space inbetween each item
     document.getElementById("underScores").innerHTML = underscoreString;
-    document.getElementById("usedLetters").innerHTML = "Guessed Letters: " + usedLetters.join(" ");
-    document.getElementById("lives").innerHTML = "Guesses Remaining: " + lives;
-    document.getElementById("wins").innerHTML = "Wins: " + wins;
-    document.getElementById("losses").innerHTML = "Losses: " + losses;
+    document.getElementById("usedLetters").innerHTML = "Guessed Letters: " + hangMan.usedLetters.join(" ");
+    document.getElementById("lives").innerHTML = "Guesses Remaining: " + hangMan.lives;
+    document.getElementById("wins").innerHTML = "Wins: " + hangMan.wins;
+    document.getElementById("losses").innerHTML = "Losses: " + hangMan.losses;
 
 };
